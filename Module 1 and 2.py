234173e8b672bff9b0edcb76fac9eedab0269010
DistinguishedG8 <distinguishedg8@gmail.com>
2024-06-15 11:58:54 -0400

    Add files via upload

---
+++ Module 1 and 2.py
@@ -0,0 +1,33 @@
+
+print("hello world")
+#First program created, displays the text "hello world".
+print("--------") #Break
+
+#Second program
+print('The capybara is the worlds largest rodent')
+print('The capybara likes to live in groups')
+print("The capybara likes to swim")
+#Display text, tesitng the print command
+print("--------") #Break
+
+print("You can use single quotes within \n John's text")
+#The \n is used to break up the line and shift the second half down one
+print("--------") #Break
+
+print("""Testing triple quotes
+ Will it print\n how I type the text?""")
+#Testing the triple quote setup
+print("--------") #Break
+
+print("Here is a single quote'" + 'Here is a double qoute"')
+print("Or you can do it like \" this")
+#Testing using different methods to keep the quotes
+print("--------") #Break
+
+print("Testing the slash with \\news")
+#Didit print backslash \news? YES
+print("--------") #Break
+
+print("There once was a movie star icon \n who preferred to sleep with the light on.\n They learned how to code \n a device that sure glowed \n and lit up the night using Python!")
+#Challenge to  break up lines
+print("--------") #Break
