e64b64c12e7096f7c571942808cd2cfbc1c8c9f0
DistinguishedG8 <distinguishedg8@gmail.com>
2024-07-04 12:00:51 -0400

    Add files via upload

---
+++ Class Testing.py
@@ -0,0 +1,47 @@
+
+# <*> Testing Cloud <*>
+
+# -> Imports
+import re
+import customtkinter
+import os
+import csv
+
+
+class dog:
+    def cat():
+        print("meow")
+    sound = "bark"
+
+
+class App(customtkinter.CTk):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("500x400")
+
+        self.button_1 = customtkinter.CTkButton(self, text="open toplevel", command=self.open_toplevel)
+        self.button_1.pack(side="top", padx=20, pady=20)
+
+        self.button_2 = customtkinter.CTkButton(self, text="sup world", command=self.open_toplevel)
+        self.button_2.pack(side="top", padx=20, pady=20)
+
+        self.toplevel_window = None
+
+    def open_toplevel(self):
+        if self.toplevel_window is None or not self.toplevel_window.winfo_exists():
+            self.toplevel_window = ToplevelWindow(self)  # create window if its None or destroyed
+        else:
+            self.toplevel_window.focus()  # if window exists focus it
+
+
+class ToplevelWindow(customtkinter.CTkToplevel):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.geometry("400x300")
+
+        self.label = customtkinter.CTkLabel(self, text="ToplevelWindow")
+        self.label.pack(padx=20, pady=20)
+
+
+app = App()
+app.mainloop()
